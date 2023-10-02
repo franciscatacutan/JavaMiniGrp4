e9f28c2e2bdbffda1d58e42d677f543dbe7f8228
rai-gd <raiguiad@gmail.com>
2023-10-02 14:04:29 +0800

    initial Cinema.java

--- src/Cinema.java
+++ src/Cinema.java
@@ -1,3 +1,24 @@
+import java.util.ArrayList;
+
 public class Cinema {
-    
+    private int cinemaNum;
+    private ArrayList<Movie> movies;
+
+
+    public int getCinemaNum() {
+        return this.cinemaNum;
+    }
+
+    public void setCinemaNum(int cinemaNum) {
+        this.cinemaNum = cinemaNum;
+    }
+
+    public ArrayList<Movie> getMovies() {
+        return this.movies;
+    }
+
+    public void setMovies(ArrayList<Movie> movies) {
+        this.movies = movies;
+    }
+
 }
