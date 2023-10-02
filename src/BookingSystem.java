648f06d957cbcc37ecb6b4c239af9e18d299ff28
franciscatacutan <fml.catacutan@gmail.com>
2023-10-02 13:53:50 +0800

    New Branch

--- src/BookingSystem.java
+++ src/BookingSystem.java
@@ -1,8 +1,13 @@
-package src;
+
 public class BookingSystem {
 
     public static void main(String[] args) {
         System.out.println("Elijah");
     }
-    
+
+    public String reserveSeat(String seatNum, Movie movie) {
+        
+        return "you";
+    }
+
 }
