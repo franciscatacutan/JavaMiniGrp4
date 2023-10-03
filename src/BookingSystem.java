12039a70395ddd77ea7b60d1b66cb8635547ef25
Elijah Capudoy <112944853+desudesux@users.noreply.github.com>
2023-10-03 13:23:22 +0800

    version1?

--- src/BookingSystem.java
+++ src/BookingSystem.java
@@ -1,8 +1,171 @@
-package src;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.List;
+import java.util.Scanner;
+
 public class BookingSystem {
+    private ArrayList<Movie> movies;
+
+    public BookingSystem() {
+        this.movies = new ArrayList<>();
+    }
 
     public static void main(String[] args) {
-        System.out.println("Elijah");
+        BookingSystem bookingSystem = new BookingSystem();
+
+        // Sample movie data
+        Date showingDate = parseDate("2023-06-01 12:45");
+        Movie movie1 = new Movie(showingDate, 1, showingDate, false, "Kung Fu Panda 2", 1.75);
+        Movie movie2 = new Movie(showingDate, 2, showingDate, true, "Avengers: Endgame", 3.0);
+
+        bookingSystem.movies.add(movie1);
+        bookingSystem.movies.add(movie2);
+
+        while (true) {
+            displayMainMenu();
+            int choice = getUserChoice();
+
+            switch (choice) {
+                case 1:
+                    bookingSystem.reserveSeat();
+                    break;
+                case 2:
+                    // Implement Cancel Seat function here
+                    break;
+                case 3:
+                    // Implement View Schedule function here
+                    break;
+                case 4:
+                    System.out.println("Exiting the booking system. Goodbye!");
+                    System.exit(0);
+                    break;
+                default:
+                    System.out.println("Invalid choice. Please select a valid option.");
+            }
+        }
+    }
+
+    private static void displayMainMenu() {
+        System.out.println("\n********** Main Menu **********");
+        System.out.println("1. Reserve Seat");
+        System.out.println("2. Cancel Seat");
+        System.out.println("3. View Schedule");
+        System.out.println("4. Exit");
+        System.out.print("Select an option: ");
+    }
+
+    private static int getUserChoice() {
+        Scanner scanner = new Scanner(System.in);
+        return scanner.nextInt();
+    }
+
+    private void reserveSeat() {
+        Scanner scanner = new Scanner(System.in);
+    
+        // Display the ongoing/showing movies and their showtimes
+        System.out.println("\nTitle Screen (Screen 1)");
+        System.out.println("***********NOW SHOWING*************");
+    
+        // Iterate through the available movies and show their titles and showtimes
+        int movieChoice = 1;
+        for (Movie movie : movies) {
+            System.out.println("[" + movieChoice + "] " + movie.getMovieInfo());
+            SimpleDateFormat timeFormat = new SimpleDateFormat("h:mm a");
+            for (Date showtime : movie.getShowtimes()) {
+                System.out.print("{" + timeFormat.format(showtime) + "} ");
+            }
+            System.out.println();
+            movieChoice++;
+        }
+    
+        System.out.print("\nChoose Movie: ");
+        int selectedMovieIndex = scanner.nextInt();
+        if (selectedMovieIndex >= 1 && selectedMovieIndex <= movies.size()) {
+            Movie selectedMovie = movies.get(selectedMovieIndex - 1);
+    
+            // Display the selected movie title and cinema number
+            System.out.println("\n*******" + selectedMovie.getMovieInfo() + "*******");
+            System.out.println("Cinema Number: " + selectedMovie.getCinemaNum());
+    
+            // Display available showtimes for the selected movie
+            ArrayList<Date> showtimes = selectedMovie.getShowtimes();
+            for (int i = 0; i < showtimes.size(); i++) {
+                System.out.println("[" + (i + 1) + "] " + new SimpleDateFormat("h:mm a").format(showtimes.get(i)));
+            }
+    
+            System.out.print("Choose Time Slot: ");
+            int selectedTimeSlot = scanner.nextInt();
+    
+            if (selectedTimeSlot >= 1 && selectedTimeSlot <= showtimes.size()) {
+                Date selectedShowtime = showtimes.get(selectedTimeSlot - 1);
+    
+                // Display the selected time slot
+                System.out.println("Time Slot: " + new SimpleDateFormat("h:mm a").format(selectedShowtime));
+    
+                // Display seat availability and occupancy for the selected movie and time slot
+                selectedMovie.displaySeatAvailability(selectedShowtime);
+    
+                // Prompt staff to input seats to reserve
+                System.out.print("\nInput Seats to Reserve for this Transaction (e.g., A1, A2, A3): ");
+                scanner.nextLine(); // Consume the newline
+                String selectedSeatsInput = scanner.nextLine().trim();
+                String[] selectedSeats = selectedSeatsInput.split(", ");
+    
+                // Update seat occupancy for the selected time slot
+                selectedMovie.setSeatOccupancy((List<String>) Arrays.asList(selectedSeats));
+    
+                // Ask if they have a Senior Citizen or PWD Card
+                System.out.print("Do you have a Senior Citizen or PWD Card? (Yes/No): ");
+                String hasCard = scanner.nextLine().trim();
+    
+                if (hasCard.equalsIgnoreCase("yes")) {
+                    // If they have a card, prompt for the quantity and card ID
+                    System.out.print("Quantity Senior Citizens / PWDs: ");
+                    int quantity = scanner.nextInt();
+                    scanner.nextLine(); // Consume the newline
+                    System.out.print("Please input Senior Citizen Card / PWD Card ID Number: ");
+                    String cardId = scanner.nextLine().trim();
+    
+                    // Proceed to checkout or cancel
+                    System.out.print("[1] Proceed to Checkout>>> ");
+                    int checkoutChoice = scanner.nextInt();
+                    if (checkoutChoice == 1) {
+                        // Implement checkout logic here
+                        // You have all the necessary information to complete the reservation
+                    } else {
+                        System.out.println("Transaction canceled.");
+                    }
+                } else {
+                    // Proceed to checkout or cancel without a card
+                    System.out.print("[1] Proceed to Checkout>>> ");
+                    int checkoutChoice = scanner.nextInt();
+                    if (checkoutChoice == 1) {
+                        // Implement checkout logic here
+                        // You have all the necessary information to complete the reservation
+                    } else {
+                        System.out.println("Transaction canceled.");
+                    }
+                }
+            } else {
+                System.out.println("Invalid time slot choice. Please select a valid option.");
+            }
+        } else {
+            System.out.println("Invalid movie choice. Please select a valid option.");
+        }
     }
     
+
+    // Helper method to parse dates
+    private static Date parseDate(String dateStr) {
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm");
+        try {
+            return dateFormat.parse(dateStr);
+        } catch (ParseException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
 }
--- src/Movie.java
+++ src/Movie.java
@@ -1,1 +1,133 @@
-package src;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.Random;
+
+public class Reservation {
+    private double pricePerSeat; // Price per seat based on movie type
+    private ArrayList<String> seats;
+    private String movieTitle;
+    private int cinemaNumber;
+    private Date showingDate;
+    private String time;
+    private int numberOfTickets;
+    private int numberOfDiscountedTickets;
+    private boolean isPremiere; // Add this field
+
+    public Reservation(String movieTitle, int cinemaNumber, Date showtime, boolean isPremiere) {
+        this.movieTitle = movieTitle;
+        this.cinemaNumber = cinemaNumber;
+        this.showingDate = showtime;
+        this.time = new SimpleDateFormat("h:mm a").format(showtime);
+        this.seats = new ArrayList<>();
+        this.isPremiere = isPremiere; // Set the isPremiere field
+
+        // Set the price per seat based on whether it's a premiere movie
+        if (isPremiere) {
+            this.pricePerSeat = 500.0;
+        } else {
+            this.pricePerSeat = 350.0;
+        }
+    }
+
+    public Reservation(String movieTitle2, int cinemaNum, Date showtime) {
+    }
+
+    public void setMovieTitle(String movieTitle) {
+        this.movieTitle = movieTitle;
+    }
+
+    public void setCinemaNumber(int cinemaNumber) {
+        this.cinemaNumber = cinemaNumber;
+    }
+
+    public void setShowingDate(Date showingDate) {
+        this.showingDate = showingDate;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    public void setNumberOfTickets(int numberOfTickets) {
+        this.numberOfTickets = numberOfTickets;
+    }
+
+    public void setNumberOfDiscountedTickets(int numberOfDiscountedTickets) {
+        this.numberOfDiscountedTickets = numberOfDiscountedTickets;
+    }
+
+    public void reserveSeats(ArrayList<String> seats) {
+        // Perform seat reservation logic here
+        this.seats.addAll(seats);
+    }
+
+    public double calculateSubtotal() {
+        // Calculate the subtotal based on the number of reserved seats
+        return pricePerSeat * seats.size();
+    }
+
+    public double calculateDiscountAmount() {
+        // Calculate the discount amount based on the number of discounted tickets (20% for senior citizens)
+        if (numberOfDiscountedTickets > 0) {
+            double discount = 0.20; // 20% discount
+            return discount * pricePerSeat * numberOfDiscountedTickets;
+        }
+        return 0.0;
+    }
+
+    public double calculateTotalAmount() {
+        // Calculate the total amount considering the discount
+        double subtotal = calculateSubtotal();
+        double discountAmount = calculateDiscountAmount();
+        return subtotal - discountAmount;
+    }
+
+    public String generateTransactionReferenceNumber() {
+        // Generate a random transaction reference number
+        Random random = new Random();
+        int referenceNumber = random.nextInt(1000000); // Adjust as needed
+        return String.format("%06d", referenceNumber); // Format as a 6-digit string
+    }
+
+    public String generateSummary() {
+        SimpleDateFormat dateFormat = new SimpleDateFormat("MMMM d, yyyy");
+        StringBuilder summary = new StringBuilder();
+
+        summary.append("*****************SUMMARY*****************\n");
+        summary.append("Movie Title:\n").append(movieTitle).append("\n");
+        summary.append("Cinema Number:\n").append(cinemaNumber).append("\n");
+        summary.append("Date:\n").append(dateFormat.format(showingDate)).append("\n");
+        summary.append("Time:\n").append(time).append("\n");
+        summary.append("Number of Ticket/s:\n").append(numberOfTickets).append("\n");
+        summary.append("Seats Reserved:\n").append(seats).append("\n");
+        summary.append("Subtotal:\n₱").append(String.format("%.2f", calculateSubtotal())).append("\n");
+        summary.append("Discount Amount:\n₱").append(String.format("%.2f", calculateDiscountAmount())).append("\n");
+        summary.append("Total Amount:\n₱").append(String.format("%.2f", calculateTotalAmount())).append("\n");
+
+        return summary.toString();
+    }
+
+    public String generateReceipt() {
+        StringBuilder receipt = new StringBuilder();
+
+        receipt.append("Receipt for ").append(isPremiere ? "Premiere" : "Regular").append(" Movie (").append(cinemaNumber).append(")\n\n");
+        receipt.append("Cinema World\n");
+        receipt.append("Transaction Reference Number:\n").append(generateTransactionReferenceNumber()).append("\n");
+        receipt.append("Movie Title:\n").append(movieTitle).append("\n");
+        receipt.append("Cinema Number:\n").append(cinemaNumber).append("\n");
+        receipt.append("Date:\n").append(showingDate).append("\n");
+        receipt.append("Time:\n").append(time).append("\n");
+        receipt.append("Number of Ticket/s:\n").append(numberOfTickets).append("\n");
+        receipt.append("Number of Discounted Tickets:\n").append(numberOfDiscountedTickets).append("\n");
+        receipt.append("Seats Reserved:\n").append(seats).append("\n");
+        receipt.append("Subtotal:\n₱").append(String.format("%.2f", calculateSubtotal())).append("\n");
+        receipt.append("Discount Amount:\n₱").append(String.format("%.2f", calculateDiscountAmount())).append("\n");
+        receipt.append("Total Amount:\n₱").append(String.format("%.2f", calculateTotalAmount())).append("\n\n");
+        receipt.append("**END\n\n");
+        receipt.append("[1] <<<Make Another Transaction?\n");
+        receipt.append("[2] Exit >>>");
+
+        return receipt.toString();
+    }
+}
