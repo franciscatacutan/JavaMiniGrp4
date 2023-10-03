5bbc0c6cbd4dadd9d9291154f3350669035bd40e
zyfayu <c.longbuankhenn@gmail.com>
2023-10-03 14:17:51 +0800

    Changed: readMovieFile() Added: containsNull()

--- src/FileContentHandler.java
+++ src/FileContentHandler.java
@@ -1,35 +1,37 @@
-import java.io.BufferedReader;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.io.IOException;
 import java.io.FileReader;
-import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class FileContentHandler {
     public ArrayList<Movie> readMovieFile() {
         ArrayList<Movie> movieList = new ArrayList<>();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+
         try {
             Scanner file = new Scanner(new FileReader("Resources/Movies.csv"));
-            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
-
 
             while (file.hasNextLine()) {
                 String line = file.nextLine();
                 String[] movieData = line.split(",");
-                if(movieData!=null && movieData.length == 6){
-                    LocalDateTime showingDate = LocalDateTime.parse(movieData[0], formatter);
-                    int cinemaNum = Integer.parseInt(movieData[1]);
-                    LocalDateTime timeStart = LocalDateTime.parse(movieData[2]);
-                    boolean isPremier = Boolean.parseBoolean(movieData[3]);
-                    String movieTitle = movieData[4];
-                    double movieTimeDuration = Double.parseDouble(movieData[5]);
-                    int[][] seats = new int[4][4];
+                if (movieData.length == 6 && !containsNull(movieData)) {
+                    LocalDateTime showingDate = LocalDate.parse(movieData[1].replace("\"", ""), formatter).atStartOfDay();
+                    int cinemaNum = Integer.parseInt(movieData[1].replace("\"", ""));
+
+                    LocalDateTime timeStart = LocalDateTime.parse(movieData[2].replace("\"", ""));
+
+                    boolean isPremier = Boolean.parseBoolean(movieData[3].replace("\"", ""));
+                    String movieTitle = movieData[4].replace("\"", "");
+                    double movieTimeDuration = Double.parseDouble(movieData[5].replace("\"", ""));
+                    int[][] seats = new int[5][8];
 
                     Movie movie = new Movie(showingDate, cinemaNum, timeStart, isPremier, movieTitle, movieTimeDuration, seats);
-                } else {
-                    System.out.println("Data has invalid/null value, Please try another file"); // To add additional if have time
+                    movieList.add(movie);
+                } else { // Null Value
+                    System.out.println("Data has invalid/null value, Please try another file"); // To add additional if still have time
                 }
             }
             file.close();
@@ -37,5 +39,13 @@ public class FileContentHandler {
             System.out.println("File can't be read: " + e);
         }
         return movieList;
+    }
+    private boolean containsNull(String[] array) {
+        for (String value : array) {
+            if (value == null || value.trim().isEmpty()) {
+                return true;
+            }
+        }
+        return false;
     }
 }
